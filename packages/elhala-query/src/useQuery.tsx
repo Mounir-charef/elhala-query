99bf0594e5818752c75bb73a35a68b5e870e4970
Mounir <polomnr1@gmail.com>
2025-02-01 23:42:24 +0100

    add: useQuery ( staleTime still not working )

---
+++ packages/elhala-query/src/useQuery.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useReducer, useRef } from "react";
+import { useElhalaClient } from "./client-provider";
+import { QueryOptions } from "./types";
+import { createQueryObserver } from "./queryObserver";
+
+export function useQuery<T>(options: QueryOptions<T>): T | undefined {
+  const client = useElhalaClient();
+
+  const [renders, rerender] = useReducer((x: number) => x + 1, 0);
+
+  const observer = useRef(createQueryObserver(client, options));
+
+  useEffect(() => {
+    console.log("subscribing", renders);
+    if (!observer.current) {
+      return;
+    }
+    return observer.current.subscribe(rerender);
+  }, [renders, observer]);
+
+  return observer.current.getResults();
+}
