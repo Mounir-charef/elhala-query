0ba93c8e872d4f25c3a68dc9636770965fe12102
Mounir <polomnr1@gmail.com>
2025-02-02 00:46:40 +0100

    draft useQuery without useSyncExternalStore yet

--- apps/www/src/App.tsx
+++ apps/www/src/App.tsx
@@ -1,22 +1,20 @@
 import { useQuery } from "elhala-query";
-import Child from "./child";
 
 function App() {
   const state = useQuery({
     queryKey: ["hello"],
     queryFn: async () => {
+      console.log("fetching data");
       await new Promise((resolve) => setTimeout(resolve, 1000));
       return "world";
     },
+    staleTime: 1000,
+    cacheTime: 2000,
   });
 
   return (
     <div className="App">
       {state.status === "loading" ? "Loading..." : state.data}
-      {/* 10 <Child /> */}
-      {Array.from({ length: 10 }).map((_, i) => (
-        <Child key={i} />
-      ))}
     </div>
   );
 }
--- packages/elhala-query/src/client-provider.tsx
+++ packages/elhala-query/src/client-provider.tsx
@@ -1,4 +1,10 @@
-import { useContext, createContext, ReactNode } from "react";
+import {
+  useContext,
+  createContext,
+  ReactNode,
+  useCallback,
+  useEffect,
+} from "react";
 import { ElhalaClient } from "./elhalaClient";
 
 const elhalaClientContext = createContext<ElhalaClient | null>(null);
@@ -20,6 +26,21 @@ export const ElhalaProvider = ({
   client: ElhalaClient;
   children: ReactNode;
 }) => {
+  const onFocus = useCallback(() => {
+    client.queries.forEach((query) => {
+      query.subscribers.forEach((sub) => sub.notify());
+    });
+  }, [client]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    window.addEventListener("focus", onFocus, { signal: controller.signal });
+    window.addEventListener("visibilitychange", onFocus, {
+      signal: controller.signal,
+    });
+
+    return () => controller.abort();
+  }, [onFocus]);
   return (
     <elhalaClientContext.Provider value={client}>
       {children}
